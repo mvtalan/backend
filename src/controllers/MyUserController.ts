539a9ad61f16c22647f2d562dd0032514a05d449
mvtalan <mvtalan@gmail.com>
2024-11-15 21:32:43 -0700

    added user profile form

--- src/controllers/MyUserController.ts
+++ src/controllers/MyUserController.ts
@@ -1,4 +1,4 @@
-import { Request, RequestHandler, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import User from "../models/user";
 
 
@@ -8,7 +8,8 @@ const createCurrentUser: RequestHandler = async (req: Request, res: Response) =>
       const existingUser = await User.findOne({ auth0Id });
   
       if (existingUser) {
-        return ;
+        res.status(200).send();
+        return;
       }
   
       const newUser = new User(req.body);
@@ -21,8 +22,33 @@ const createCurrentUser: RequestHandler = async (req: Request, res: Response) =>
     }
   };
 
+  const updateCurrentUser = async (req: Request, res: Response,next: NextFunction) /*: Promise<void>*/ => {
+    try {
+      const { name, addressLine1, country, city } = req.body;
+      const user = await User.findById(req.userId);
+  
+      if (!user) {
+        // return res.status(404).json({ message: "User not found" });
+        return;
+      }
+  
+      user.name = name;
+      user.addressLine1 = addressLine1;
+      user.city = city;
+      user.country = country;
+  
+      await user.save();
+  
+      res.json(user);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error updating user" });
+      // next(error);
+    }
+  };
+
   export default {
 
     createCurrentUser,
-
+    updateCurrentUser,
   };
